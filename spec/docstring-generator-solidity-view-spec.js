3a48b0778e260e729286d2317a36cea95e09c34f
Bruce Li <ryu9827@gmail.com>
2018-04-24 13:38:59 +1200

    first commit

---
+++ spec/docstring-generator-solidity-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import DocstringGeneratorSolidityView from '../lib/docstring-generator-solidity-view';
+
+describe('DocstringGeneratorSolidityView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
